1e9c9066f7694f1696211630d6e64e5e3c8c6266
jonasv-mps <jonas.valtersson@mobilepaymentsolutions.se>
2015-11-19 13:47:30 +0100

    removed junk

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,17 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name mehadminApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the mehadminApp
- */
-angular.module('mehadminApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('mehadminApp').controller('mapController', function () {
+
+ });
