78f5a126d3a4cc28c1a92cb3d653dab46662491d
jonasv-mps <jonas.valtersson@mobilepaymentsolutions.se>
2015-11-19 14:09:10 +0100

    contrller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,43 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name mehadminApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the mehadminApp
- */
-angular.module('mehadminApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('mehadminApp').controller('MainCtrl', function (h337) {
+
+	function listStores() {
+		var list = [
+			{name: 'Store 1', locationX: 5, locationY: 10, hits: 38},
+			{name: 'Store 2', locationX: 15, locationY: 12, hits: 28},
+			{name: 'Store 3', locationX: 35, locationY: 14, hits: 18}
+		];
+	}
+
+	var heatmapInstance = h337.create({
+	  // only container is required, the rest will be defaults
+	  container: document.querySelector('.heatmap')
+	});
+
+	// now generate some random data
+	var points = [];
+	var max = 0;
+	var width = 840;
+	var height = 400;
+	var len = 200;
+
+	while (len--) {
+	  var val = Math.floor(Math.random()*100);
+	  max = Math.max(max, val);
+	  var point = {
+	    x: Math.floor(Math.random()*width),
+	    y: Math.floor(Math.random()*height),
+	    value: val
+	  };
+	  points.push(point);
+	}
+	// heatmap data format
+	var data = { 
+	  max: max, 
+	  data: points 
+	};
+	// if you have a set of datapoints always use setData instead of addData
+	// for data initialization
+	heatmapInstance.setData(data);
+});
